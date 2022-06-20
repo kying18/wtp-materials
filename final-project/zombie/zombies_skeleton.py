5a304a76194b7b2ffad8364468252649f6b2f547
Zoe <zpenguin@mit.edu>
2022-06-20 15:06:52 -0700

    finished skeleton for person class

--- final-project/zombie/zombies_skeleton.py
+++ final-project/zombie/zombies_skeleton.py
@@ -1,99 +1,120 @@
 import random
-sus_range = (1, 1) #tuple of low and high bounds for susceptibility
+import numpy as np
+strength_range = (0, 0.5)  # tuple of low and high bounds for resistance
+cure_time = (10, 4) #the first value is the mean, then second is the 'mu' or standard deviation: ask a TA if you have any questions!
+
 
 class Person(object):
     def __init__(self, state):
         '''
         Initial constructor of a person. Holds their susceptibility (float, range 0:1), and state.
-        'I' = infected (aka zombie)
         'S' = susceptible (human)
-        'R' = recovered + immune (cured and incapable of becoming a zombie again)
+        'I' = infected (aka zombie)
+        'R' = recovered + immune (cured and incapable of becoming a zombie again, represented by a robot)
         '''
-        self.sus = random.uniform(*sus_range) #unpacking notation
-        #equivalent to = random.uniform(sus_range[0], sus_range[1])
+        self.strength = random.uniform(*strength_range)  # unpacking notation
+        # equivalent to = random.uniform(strength_range[0], strength_range[1])
+        self.recovery_time = random.gauss(*cure_time)
+        #uses a normal distribution based on the cure_time tuple above with the first value as the mean, second as the standard deviation
 
-        #what other statement do we need here?
+
+        #TO_DO: write a line that creates a variable 'num_rounds' for EACH student, and sets that variable to zero
+
+        #TO_DO: bind the 'state' variable to the student object
 
     def interact(self):
         '''
         returns a boolean based on whether or not an interaction with an infected student results in an infection
         "R" = resistant and no infection will occur
         '''
-        if self.get_state() == 'R':
-           ##return what here
-        viral_strength = random.random() #random value of infection strength
-        student_strength = 1 - self.sus
-        if student_strength < viral_strength: #if student strength is less than virus strength, infected
-            ##what about this case
-        ##return something here??
+        viral_strength = random.random()  # random value of infection strength
+        ###
+        #TO_DO: write code that returns the correct value if the student is resistant
+        ###
+
+        #TO_DO: compare student and virus strength. If the virus is stronger, they are infected
+        # if student strength is less than virus strength, infected
+
+        #Note: make sure that your code returns a BOOLEAN (not None), in every possible scenario
 
     def get_state(self):
         '''
-        Returns the state, either 'I', 'S', or 'R'
+        Returns the state, either 'S', 'I' or 'R'
         :return:
         '''
-        ###
-        #What can we write to get the state of a student?
-        ###
+        #TO_DO
 
     def set_state(self, state):
         '''
-        Sets the state given 'I', 'S', or 'R'
+        Sets the state given 'S', 'I', or 'R'
         '''
-        ###
-        #How can we *set* the state of a student
+        #TO_DO
+
 
 class School(object):
-    def __init__(self, num_students, initial_spread, beta=2): #discuss including variable name in header?
+    def __init__(self, num_students, initial_spread, beta=2):  # discuss including variable name in header?
         '''
         Constructs a school given a number of students and the initial probability of infection.
         '''
-        ##Bind beta here
+        self.beta = beta
         self.list_students = []
-        #creating a list of students, with a random chance of the disease
-        for student in range(num_students):
-            rand_disease = random.choices(['S', 'I'], weights=[1-initial_spread, initial_spread])[0] #weighted choices, unpacking list
+        for _ in range(num_students):
+            # weighted choices, unpacking list
+            rand_disease = random.choices(['S', 'I'], weights=[1-initial_spread, initial_spread])[0]
             self.list_students.append(Person(rand_disease))
-        self.school_size = ### what?
+        self.school_size = num_students
 
     def infect_round(self):
         '''
         Simulates a day of student interactions, controlled by the sociality variable 'BETA"
         '''
-        #generate a list of infected students
-
-        ##TO_DO
-
-        #for each infected student, have them interact with some random subset of students
+        # generate a list of infected students
+        infected_students = []
+        for student in self.list_students:
+            if student.get_state() == 'I':
+                infected_students.append(student)
+        # for each infected student, have them interact with some random subset of students
         for _ in infected_students:
-            for i in range(round(self.beta)): #in this code, beta can be a float
+            #the underscore (_) is a python convention: when we don't use the VALUE of a term (like student in infected students)
+            #we don't put a real term there, and it's just like a counter (equivalent to say for i in range(len(infected_students)
+            for i in range(round(self.beta)):  # in this code, beta can be a float
                 print('infecting student', i, 'for beta', self.beta)
                 contact = random.randint(0, self.school_size-1)
                 infected = self.list_students[contact].interact()
                 if infected:
                     self.list_students[contact].set_state('I')
+        for student in self.list_students:
+            student.num_rounds += 1
+            if student.num_rounds >= student.recovery_time:
+                student.set_state('R')
 
     def get_num_infected(self):
         '''
         Returns number of zombie students as an integer
         '''
-        ##TO_DO implementation
+        count = 0
+        for student in self.list_students:
+            if student.get_state() == 'I':
+                count += 1
+        return count
 
     def can_continue(self):
         if self.school_size == self.get_num_infected():
             return False
+            #if everyone is infected, game over
         robo_count = 0
-        #generate count of resistant/robot students
-
-        ## TO_DO ##
-
+        for student in self.list_students:
+            if student.get_state() == 'R':
+                robo_count += 1
         if robo_count == self.school_size:
             return False
+            #if everyone is cured, game won
         else:
             return True
+            #otherwise keep playing
 
     ###
-    #METHODS TO LOWER INFECTION RATES
+    # METHODS TO LOWER INFECTION RATES
     ###
     def cure_student(self):
         '''
@@ -108,22 +129,27 @@ class School(object):
                 if student.get_state() == 'I':
                     student.set_state('R')
                     return None
+
     def lower_beta(self):
         '''
         Lowers the number of students each infected individual interacts with to 80% of current levels
         :return:
         '''
-        ##TO_DO implement
+        self.beta = self.beta*0.8
 
     def strengthen_students(self):
         '''
         Reduces the susceptibility of all students by 10%
         '''
-        ##TO_DO implement
+        for student in self.list_students:
+            student.sus = 0.9*student.sus
 
-
-
-
+    def shorten_cure_time(self):
+        '''
+        Shortens the time to cure of all students by 10%
+        '''
+        for student in self.list_students:
+            student.recovery_time = 0.9*student.recovery_time
 
     def __str__(self):
         '''
@@ -131,24 +157,21 @@ class School(object):
         '''
         string = ''
         total_length = len(self.list_students)
-        repr_dict = {'S': 'o', 'I': 'z', 'R': 'R'} #shows o for students and z for zombies
+        repr_dict = {'S': 'o', 'I': 'z', 'R': 'R'}  # shows o for students and z for zombies
         for ix in range(total_length):
-            string += repr_dict[self.list_students[ix].get_state()] + ' ' #adds each student
-            if (ix+1) % 15 == 0: #line breaks every 15 students
+            string += repr_dict[self.list_students[ix].get_state()] + ' '  # adds each student
+            if (ix+1) % 15 == 0:  # line breaks every 15 students
                 string += '\n'
         return string
 
-
-    def __repr__(self):
-        for student in self.list_students:
-            print('Student', student.get_state())
 
 if __name__ == "__main__":
     '''
     Runs a school simulation of size 75 with an initial infection chance of 1/20
     '''
     school = School(75, 0.05)
-    start = input('Welcome, school administrator. Your school has been infected with a zombie virus. Type \'YES\' to start the simulation.')
+    start = input(
+        'Welcome, school administrator. Your school has been infected with a zombie virus. Type \'YES\' to start the simulation.')
     if start in ['YES', 'yes', 'Y', 'y']:
         print(school)
         num_rounds = 0
@@ -165,5 +188,3 @@ if __name__ == "__main__":
                 if school.get_num_infected() == school.school_size:
                     print('All your students are now zombies. Your school survived', num_rounds-1, 'rounds')
                     exit()
-
-
