5a9ed52fbdf85e40558de9f77e0baa89424c54b6
Adeena Khan <adeena@mit.edu>
2022-07-06 12:57:40 -0500

    update to pdf and solutions

--- final-project/Image_Manipulation/solutions.py
+++ final-project/Image_Manipulation/solutions.py
@@ -108,7 +108,27 @@ def apply_kernel(image, kernel):
     # [1 0 -1]
     # [2 0 -2]
     # [1 0 -1]
-    pass
+        x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
+        temp_Image = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
+        kernel_size=kernel.shape[0]
+        neighbor_range = kernel_size // 2  # this is a variable that tells us how many neighbors we actually look at (ie for a kernel of 3, this value should be 1)
+        for i in range(x_pixels):
+            for z in range(y_pixels):
+                for l in range(num_channels):
+                # we are going to use a naive implementation of iterating through each neighbor and summing
+                # there are faster implementations where you can use memoization, but this is the most straightforward for a beginner to understand
+                    total = 0
+                    for x_2 in range(-neighbor_range,neighbor_range+1):
+                        for y_2 in range(-neighbor_range,neighbor_range+1):
+                             x_kernel = x_2+neighbor_range
+                             y_kernel = y_2+neighbor_range
+                             value=kernel[x_kernel,y_kernel]
+                             if (x_2+i>0 and x_2+i<x_pixels) and (y_2+z>0 and y_2+z<y_pixels):
+                                total+=image.array[x_2+i,y_2+z,l]*value
+                             else:
+                                continue
+                    temp_Image.array[i,z,l]=total
+        return temp_Image
 
 def combine_images(image1, image2):
     temp_Image=Image(image1.array.shape[0],image1.array.shape[1],image1.array.shape[2])
@@ -158,24 +178,7 @@ def create_a_border(image1,image2,start_pixls,dimension):
     return temp_Image
 
 
-def blur2(image, kernel_size):
-    # kernel size is the number of pixels to take into account when applying the blur
-    # (ie kernel_size = 3 would be neighbors to the left/right, top/bottom, and diagonals)
-    # kernel size should always be an *odd* number
-    x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
-    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
-    neighbor_range = kernel_size // 2  # this is a variable that tells us how many neighbors we actually look at (ie for a kernel of 3, this value should be 1)
-    for x in range(x_pixels):
-        for y in range(y_pixels):
-            for c in range(num_channels):
-                # we are going to use a naive implementation of iterating through each neighbor and summing
-                # there are faster implementations where you can use memoization, but this is the most straightforward for a beginner to understand
-                total = 0
-                for x_i in range(max(0,x-neighbor_range), min(new_im.x_pixels-1, x+neighbor_range)+1):
-                    for y_i in range(max(0,y-neighbor_range), min(new_im.y_pixels-1, y+neighbor_range)+1):
-                        total += image.array[x_i, y_i, c]
-                new_im.array[x, y, c] = total / (kernel_size ** 2)
-    return new_im
+
 
 #we want to flip the length of the lake, so looking for when blue starts and blue ends
 #img = PIL.Image.open("lake.png")
@@ -189,13 +192,13 @@ def blur2(image, kernel_size):
 #give kerb mid piece hat
 
 if __name__ == '__main__':
-    lonk = Image(filename="Sunflower.png")
+    lonk = Image(filename="test_images/city.png")
 
  
    
-    test=brighten(lonk,2)
+    test=apply_kernel(lonk,np.array([[1,2,1],[0,0,0],[-1,-2,-1]]))
 
-    test.write_image("bright.png")
+    test.write_image("edge_x.png")
 
 
 
